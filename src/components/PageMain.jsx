15052b36f4151b376ecb9a52faf34eb917005ec0
Shogo Ogami <sg.tao.so@gmail.com>
2023-10-21 11:50:33 +0900

    update components/PageMain for replace icon of svg format

--- src/components/PageMain.jsx
+++ src/components/PageMain.jsx
@@ -1,5 +1,17 @@
 import "./PageMain.css";
 
+const MicrophoneIcon = ({ strokeColor = 'currentColor' }) => {
+    // マイクアイコン
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48" fill="none"
+            stroke={strokeColor} stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <rect x="9" y="2" width="6" height="14" rx="3" ry="3"></rect>
+            <line x1="12" y1="18" x2="12" y2="20"></line>
+            <line x1="5" y1="20" x2="19" y2="20"></line>
+        </svg>
+    )
+}
+
 const PageMain = ({ setInfoState }) => {
     const showInfo = (s) => {
         if (s) {
@@ -15,7 +27,7 @@ const PageMain = ({ setInfoState }) => {
         <>
             <div className="div-start">
                 <button className="start-button">
-                    マイク
+                    <MicrophoneIcon strokeColor="red" />
                 </button>
             </div>
             <div className="results">
